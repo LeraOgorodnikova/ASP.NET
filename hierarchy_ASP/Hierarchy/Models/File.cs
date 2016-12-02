503c1b53852f84a6e1e97ebeb79dc31b9fedde07
LeraOgorodnikova <lera.ogorodnikovaa@yandex.ru>
2016-12-02 15:37:31 +0500

    Begin lab1

---
+++ hierarchy_ASP/Hierarchy/Models/File.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hierarchy.Models
+{
+    public class File
+    {
+        public String name { get; set; }
+        public DateTime date { get; set; }
+        public int size { get; set; }
+
+        public File(String Newname, DateTime newDate, int newSize)
+        {
+            this.name = Newname;
+            this.date = newDate;
+            this.size = newSize;
+        }
+    }
+}
---
+++ hierarchy_ASP/Hierarchy/Models/Folder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hierarchy.Models
+{
+    public class Folder
+    {
+        public String name { get; set; }
+        public DateTime date { get; set; }
+        public int child_objects { get; set; }
+        public List<File> files { get; set; }
+        public List<Folder> subFolder { get; set; }
+        public Folder(String Newname, DateTime newDate, int newChild_objects,List<File> newFiles,List<Folder> sub)
+        {
+            this.name = Newname;
+            this.date = newDate;
+            this.child_objects = newChild_objects;
+            this.files = newFiles;
+            this.subFolder = sub;
+        }
+        public Folder()
+        {
+        }
+    }
+}
