cb3a653a7b517ad3539891241626d5c949c1b785
Valeriya <lera.ogorodnikovaa@yandex.ru>
2018-02-28 23:28:39 +0500

    Lab1

---
+++ ConsoleApplication1/ConsoleApplication1/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Database: IDatabase
+    {
+        public ILogger logger { get; set; }
+
+        public Database(ILogger lg)
+        {
+            logger = lg;
+        }
+        public void Execute(string query)
+        {
+            ///code
+            
+            logger.Log(query);
+        }
+
+        public ILogger getLogger()
+        {
+            return logger;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DryIoc;
+using Ninject;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var c = new Container();
+            c.Register<ILogger, Logger>(Reuse.Singleton);
+            c.Register<IDatabase, Database>();
+           
+            IDatabase db = c.Resolve<Database>();*/
+
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IDatabase>().To<Database>().InTransientScope();
+            ninjectKernel.Bind<ILogger>().To<Logger>().InSingletonScope();
+            IDatabase db = ninjectKernel.Get<IDatabase>();
+
+            db.Execute("select * from users where id_user = 77");
+            Console.ReadLine();
+
+        }
+    }
+}
