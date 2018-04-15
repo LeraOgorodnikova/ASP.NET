27b43c044e3138e9c23f033ed732dae77be15986
Valeriya <lera.ogorodnikovaa@yandex.ru>
2018-04-16 00:12:38 +0500

    3 laba - работает

--- WebApplicationNetCore_v2/WebApplicationNetCore_v2/Controllers/ItemDescController.cs
+++ WebApplicationNetCore_v2/WebApplicationNetCore_v2/Controllers/ItemDescController.cs
@@ -5,7 +5,7 @@ using WebApplicationNetCore_v2.Models;
 
 namespace WebApplicationNetCore_v2.Controllers
 {
-    public class ItemDescription : Controller
+    public class ItemDescController : Controller
     {
         [ResponseCache(Duration = 60)]
         public IActionResult Index(int id)
